8a5a5ba5d61008055d763638ce91ee69e3937eaf
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-01 23:59:21 +0700

    chore

--- 2024/day-01/src/bin/part1.rs
+++
@@ -1,8 +0,0 @@
-use day_01::part1::solve;
-
-fn main() {
-    let input = include_str!("./input1");
-    let result = solve(input);
-
-    println!("{}", result);
-}
--- 2024/day-01/src/bin/part2.rs
+++
@@ -1,8 +0,0 @@
-use day_01::part2::solve;
-
-fn main() {
-    let input = include_str!("./input2");
-    let result = solve(input);
-
-    println!("{}", result);
-}
