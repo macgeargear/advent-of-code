ce498fb51dfe9ab3229708b4eff57e5bea3f0128
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-01 22:59:28 +0700

    feat: reorganized

--- 2024/day-01/src/bin/part1.rs
+++ 2024/day-01/src/bin/part1.rs
@@ -1,48 +1,8 @@
-use std::collections::BinaryHeap;
-
-fn part1(input: &str) -> i32 {
-    let mut sum: i32 = 0;
-    let mut pq1: BinaryHeap<i32> = BinaryHeap::new();
-    let mut pq2: BinaryHeap<i32> = BinaryHeap::new();
-
-    for line in input.lines() {
-        let nums: Vec<i32> = line
-            .split_whitespace()
-            .filter_map(|x| x.parse::<i32>().ok())
-            .collect();
-        pq1.push(nums[0]);
-        pq2.push(nums[1]);
-    }
-
-    while !pq1.is_empty() && !pq2.is_empty() {
-        let n1 = pq1.pop().unwrap();
-        let n2 = pq2.pop().unwrap();
-        sum += (n1 - n2).abs();
-    }
-
-    sum
-}
+use day_01::part1::solve;
 
 fn main() {
     let input = include_str!("./input1");
-    let result = part1(input);
+    let result = solve(input);
 
     println!("{}", result);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::part1;
-
-    #[test]
-    fn test() {
-        let input = "3   4
-                    4   3
-                    2   5
-                    1   3
-                    3   9
-                    3   3";
-        let result = part1(input);
-        assert_eq!(result, 11);
-    }
 }
--- 2024/day-01/src/bin/part2.rs
+++ 2024/day-01/src/bin/part2.rs
@@ -1,53 +1,8 @@
-use std::collections::HashMap;
+use day_01::part2::solve;
 
 fn main() {
     let input = include_str!("./input2");
-    let result = part2(input);
+    let result = solve(input);
 
     println!("{}", result);
-}
-
-fn part2(input: &str) -> i32 {
-    let mut right_map: HashMap<i32, i32> = HashMap::new();
-    let mut left: Vec<i32> = vec![];
-    let mut res: i32 = 0;
-
-    for line in input.lines() {
-        let nums: Vec<i32> = line
-            .split_whitespace()
-            .filter_map(|x| x.parse::<i32>().ok())
-            .collect();
-        left.push(nums[0]);
-        if let Some(v) = right_map.get_mut(&nums[1]) {
-            *v += 1;
-        } else {
-            right_map.insert(nums[1], 1);
-        }
-    }
-
-    for e in left.iter() {
-        if let Some(v) = right_map.get_mut(e) {
-            println!("{} * {} = {}", e, v, *e * *v);
-            res += *e * *v;
-        }
-    }
-
-    res
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let input = "3   4
-                    4   3
-                    2   5
-                    1   3
-                    3   9
-                    3   3";
-        let result = part2(input);
-        assert_eq!(result, 31);
-    }
 }
