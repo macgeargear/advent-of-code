0bda197c4a7c31a58e612710c8cf6417e8c547c6
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-02 13:20:15 +0700

    feat(day-02): part1

--- 2024/rust/daily-template/src/bin/part1.rs
+++ 2024/rust/daily-template/src/bin/part1.rs
@@ -1,7 +1,7 @@
 use {{crate_name}}::part1::solve;
 
 fn main() {
-    let input = include_str!("../input1");
+    let input = include_str!("../../input1.txt");
     let result = solve(input);
     println!("{}", result);
 }
