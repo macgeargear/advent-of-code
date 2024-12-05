c6707faa8d79220bd59df31bc7a6208457273016
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-06 00:27:53 +0700

    fix(day-05): refactor `parse_input`

--- 2024/rust/day-05/src/part1_hashset.rs
+++ 2024/rust/day-05/src/part1_hashset.rs
@@ -1,26 +1,4 @@
-use std::collections::HashSet;
-
-fn parse_input(input: &str) -> (HashSet<(i32, i32)>, Vec<Vec<i32>>) {
-    let mut rules: HashSet<(i32, i32)> = HashSet::new();
-    let mut page_orders: Vec<Vec<i32>> = Vec::new();
-    let mut empty_line = false;
-
-    for line in input.lines().map(str::trim) {
-        if line.is_empty() {
-            empty_line = true;
-            continue;
-        }
-        let sep = if empty_line { "," } else { "|" };
-        let parsed_line: Vec<i32> = line.split(sep).map(|x| x.parse::<i32>().unwrap()).collect();
-        if empty_line {
-            page_orders.push(parsed_line);
-        } else {
-            rules.insert((parsed_line[0], parsed_line[1]));
-        }
-    }
-
-    (rules, page_orders)
-}
+use crate::parse_input;
 
 pub fn solve(input: &str) -> i32 {
     let (rules, page_orders) = parse_input(input);
