a37458ec031b62e9f33cf9db51c920151a0cb262
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-16 17:50:33 +0700

    refactor(day-16): part2

--- 2024/rust/day-16/src/part2.rs
+++ 2024/rust/day-16/src/part2.rs
@@ -1,96 +1,128 @@
 use std::collections::*;
 
-fn valid(map: &Vec<Vec<char>>, r: i32, c: i32) -> bool {
-    r < map.len() as i32 && c < map[0].len() as i32 && map[r as usize][c as usize] != '#'
+struct Maze {
+    map: Vec<Vec<char>>,
+    start: (usize, usize),
+    end: (usize, usize),
+    dirs: Vec<(i32, i32)>,
 }
 
-fn get_start_end(map: &Vec<Vec<char>>) -> ((usize, usize), (usize, usize)) {
-    let mut start: (usize, usize) = (0, 0);
-    let mut stop: (usize, usize) = (0, 0);
+impl Maze {
+    fn new(input: &str) -> Self {
+        let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+        let dirs: Vec<(i32, i32)> = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
+        let (start, end) = Maze::get_start_end(&grid);
 
-    for i in 0..map.len() {
-        for j in 0..map[0].len() {
-            match map[i][j] {
-                'S' => start = (i, j),
-                'E' => stop = (i, j),
-                _ => (),
-            }
+        Self {
+            map: grid,
+            start,
+            end,
+            dirs,
         }
     }
-    (start, stop)
+
+    fn get_start_end(map: &Vec<Vec<char>>) -> ((usize, usize), (usize, usize)) {
+        let mut start: (usize, usize) = (0, 0);
+        let mut stop: (usize, usize) = (0, 0);
+
+        for i in 0..map.len() {
+            for j in 0..map[0].len() {
+                match map[i][j] {
+                    'S' => start = (i, j),
+                    'E' => stop = (i, j),
+                    _ => (),
+                }
+            }
+        }
+        (start, stop)
+    }
+
+    fn valid(&self, r: i32, c: i32) -> bool {
+        r < self.map.len() as i32
+            && c < self.map[0].len() as i32
+            && self.map[r as usize][c as usize] != '#'
+    }
+
+    fn find_all_min_path(&self) -> HashSet<(usize, usize)> {
+        // (cost, row, col, direction, cur_path)
+        let mut pq = BinaryHeap::new();
+        let mut min_paths: Vec<Vec<(usize, usize)>> = Vec::new();
+        let mut min_cost = i32::MAX;
+        let mut dist: HashMap<(usize, usize, i32), i32> = HashMap::new(); // (r, c, dir)
+
+        pq.push((0, self.start.0, self.start.1, -1, vec![self.start]));
+        dist.insert((self.start.0, self.start.1, -1), 0);
+
+        while !pq.is_empty() {
+            let (cost, r, c, d, cur_path) = pq.pop().unwrap();
+            let cost = -cost; // max heap
+            if (r, c) == self.end {
+                if cost <= min_cost {
+                    if cost < min_cost {
+                        min_cost = cost;
+                        min_paths.clear();
+                    }
+                    min_paths.push(cur_path);
+                }
+                continue;
+            }
+
+            let curr_best = dist.get(&(r, c, d)).unwrap_or(&i32::MAX);
+            if cost > *curr_best {
+                continue;
+            }
+
+            for (i, (dr, dc)) in self.dirs.iter().enumerate() {
+                let (nr, nc) = (r as i32 + dr, c as i32 + dc);
+                if !self.valid(nr, nc) {
+                    continue;
+                }
+                let (nr, nc) = (nr as usize, nc as usize);
+
+                let new_cost = if i == d as usize {
+                    cost + 1 // same direction
+                } else {
+                    cost + 1001 // change direction
+                };
+
+                if new_cost <= *dist.get(&(nr, nc, i as i32)).unwrap_or(&i32::MAX) {
+                    dist.insert((nr, nc, i as i32), new_cost);
+                    let mut new_path = cur_path.clone();
+                    new_path.push((nr, nc));
+                    pq.push((-new_cost, nr, nc, i as i32, new_path));
+                }
+            }
+        }
+
+        let mut result: HashSet<(usize, usize)> = HashSet::new();
+        for path in min_paths {
+            for pos in path {
+                result.insert(pos);
+            }
+        }
+
+        result
+    }
+
+    fn print(&self, result: &HashSet<(usize, usize)>) {
+        for i in 0..self.map.len() {
+            for j in 0..self.map[0].len() {
+                if result.contains(&(i, j)) {
+                    print!("O");
+                } else {
+                    print!("{}", self.map[i][j]);
+                }
+            }
+            println!();
+        }
+    }
 }
 
 pub fn solve(input: &str) -> i32 {
-    let map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    let dirs: Vec<(i32, i32)> = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
-    let mut pq: BinaryHeap<(i32, usize, usize, i32, Vec<(usize, usize)>)> = BinaryHeap::new();
-    let mut min_paths: Vec<Vec<(usize, usize)>> = Vec::new();
-    let mut min_cost = i32::MAX;
-    let mut dist: HashMap<(usize, usize, i32), i32> = HashMap::new();
-    let (start, end) = get_start_end(&map);
+    let maze = Maze::new(input);
 
-    pq.push((0, start.0, start.1, -1, vec![start]));
-    dist.insert((start.0, start.1, -1), 0);
-
-    while let Some((cost, r, c, d, path)) = pq.pop() {
-        let cost = -cost;
-
-        if (r, c) == end {
-            if cost <= min_cost {
-                if cost < min_cost {
-                    min_cost = cost;
-                    min_paths.clear();
-                }
-                min_paths.push(path);
-            }
-            continue;
-        }
-
-        let curr_best = dist.get(&(r, c, d)).unwrap_or(&i32::MAX);
-        if cost > *curr_best {
-            continue;
-        }
-
-        for (i, (dr, dc)) in dirs.iter().enumerate() {
-            let (nr, nc) = (r as i32 + dr, c as i32 + dc);
-            if !valid(&map, nr, nc) {
-                continue;
-            }
-            let (nr, nc) = (nr as usize, nc as usize);
-            let new_cost = if i == d as usize {
-                cost + 1
-            } else {
-                cost + 1001
-            };
-
-            if new_cost <= *dist.get(&(nr, nc, i as i32)).unwrap_or(&i32::MAX) {
-                dist.insert((nr, nc, i as i32), new_cost);
-                let mut new_path = path.clone();
-                new_path.push((nr, nc));
-                pq.push((-new_cost, nr, nc, i as i32, new_path));
-            }
-        }
-    }
-
-    // Combine all minimum cost paths into result
-    let mut result: HashSet<(usize, usize)> = HashSet::new();
-    for path in min_paths {
-        for pos in path {
-            result.insert(pos);
-        }
-    }
-
-    // Print the result map
-    // for i in 0..map.len() {
-    //     for j in 0..map[0].len() {
-    //         if result.contains(&(i, j)) {
-    //             print!("O");
-    //         } else {
-    //             print!("{}", map[i][j]);
-    //         }
-    //     }
-    //     println!();
-    // }
+    let result = maze.find_all_min_path();
+    maze.print(&result);
 
     result.len() as i32
 }
