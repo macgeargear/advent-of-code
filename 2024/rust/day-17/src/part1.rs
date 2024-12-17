b4a23a97852399361d8dc0126a8632b9a76b47b9
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-17 22:01:32 +0700

    revert(day-17): part1

--- 2024/rust/day-17/src/part1.rs
+++ 2024/rust/day-17/src/part1.rs
@@ -1,37 +1,3 @@
-#[derive(Debug)]
-enum RegisterField {
-    A,
-    B,
-    C,
-}
-
-#[derive(Debug)]
-enum Operand {
-    Literal(i32),         // 0-3 literal value
-    Combo(RegisterField), // 4-A 5-B 6-C 7-reserved
-}
-
-impl Operand {
-    fn new(op: i32) -> Self {
-        match op {
-            0..=3 => Operand::Literal(op),
-            4 => Operand::Combo(RegisterField::A),
-            5 => Operand::Combo(RegisterField::B),
-            6 => Operand::Combo(RegisterField::C),
-            _ => panic!("Invalid operand"),
-        }
-    }
-
-    fn get_value(&self, reg: &Register) -> i32 {
-        match self {
-            Operand::Literal(l) => *l,
-            Operand::Combo(RegisterField::A) => reg.a,
-            Operand::Combo(RegisterField::B) => reg.b,
-            Operand::Combo(RegisterField::C) => reg.c,
-        }
-    }
-}
-
 enum Instruction {
     Adv, // opcode 0(division): A <- division 2 ^ (combo operand) / A
     Bxl, // opcode 1(XOR): B <- B ^ (literal operand)
@@ -42,7 +8,6 @@ enum Instruction {
     Bdv, // opcode 6: same as Adv but save result in B
     Cdv, // opcode 7: same as Adv but save result in C
 }
-
 impl Instruction {
     fn new(ins: i32) -> Self {
         match ins {
@@ -58,76 +23,102 @@ impl Instruction {
         }
     }
 }
-
 #[derive(Debug)]
 struct Register {
     a: i32,
     b: i32,
     c: i32,
 }
-
 impl Register {
     fn new(a: i32, b: i32, c: i32) -> Register {
         Register { a, b, c }
     }
-
     fn process(&mut self, instructions: Vec<(i32, i32)>) -> String {
         println!("start: {:#?}", self);
         let mut result: Vec<i32> = Vec::new();
         let mut i = 0;
         while i < instructions.len() {
             let (ins, op) = instructions[i];
+            println!("ins: {} op: {}", ins, op);
+            // for (ins, op) in instructions {
             let ins = Instruction::new(ins);
-            let op = Operand::new(op);
-
             match ins {
-                Instruction::Adv => self.execute_div(op, RegisterField::A),
+                Instruction::Adv => {
+                    // A <- division 2 ^ (combo operand) / A
+                    match op {
+                        1..=3 => self.a = self.a / 2i32.pow(op as u32),
+                        4 => self.a = self.a / 2i32.pow(self.a as u32),
+                        5 => self.a = self.a / 2i32.pow(self.b as u32),
+                        6 => self.a = self.a / 2i32.pow(self.c as u32),
+                        _ => panic!("Invalid operand"),
+                    }
+                }
                 Instruction::Bxl => {
-                    self.b ^= op.get_value(self);
+                    // B <- B ^ (literal operand)
+                    self.b = self.b ^ op;
                 }
                 Instruction::Bst => {
-                    self.b = op.get_value(self) % 8;
+                    // B <- (combo operand) % 8
+                    match op {
+                        1..=3 => self.b = op % 8,
+                        4 => self.b = self.a % 8,
+                        5 => self.b = self.b % 8,
+                        6 => self.b = self.c % 8,
+                        _ => panic!("Invalid operand"),
+                    }
                 }
                 Instruction::Jnz => {
+                    // if A == 0 pass else jump to (literal operand) (ic not increased after jump)
                     if self.a != 0 {
-                        i = op.get_value(self) as usize;
+                        i = op as usize;
                         continue;
                     }
                 }
-                Instruction::Bxc => self.b ^= self.c,
-                Instruction::Out => result.push(op.get_value(self) % 8),
-                Instruction::Bdv => self.execute_div(op, RegisterField::B),
-                Instruction::Cdv => self.execute_div(op, RegisterField::C),
+                Instruction::Bxc => {
+                    // B <- B ^ C
+                    self.b = self.b ^ self.c;
+                }
+                Instruction::Out => {
+                    // print (combo operand) % 8
+                    match op {
+                        1..=3 => result.push(op % 8),
+                        4 => result.push(self.a % 8),
+                        5 => result.push(self.b % 8),
+                        6 => result.push(self.c % 8),
+                        _ => panic!("Invalid operand"),
+                    }
+                }
+                Instruction::Bdv => {
+                    // same as Adv but save result in B
+                    match op {
+                        1..=3 => self.b = self.a / 2i32.pow(op as u32),
+                        4 => self.b = self.a / 2i32.pow(self.a as u32),
+                        5 => self.b = self.a / 2i32.pow(self.b as u32),
+                        6 => self.b = self.a / 2i32.pow(self.c as u32),
+                        _ => panic!("Invalid operand"),
+                    }
+                }
+                Instruction::Cdv => {
+                    // same as Adv but save result in C
+                    match op {
+                        1..=3 => self.c = self.a / 2i32.pow(op as u32),
+                        4 => self.c = self.a / 2i32.pow(self.a as u32),
+                        5 => self.c = self.a / 2i32.pow(self.b as u32),
+                        6 => self.c = self.a / 2i32.pow(self.c as u32),
+                        _ => panic!("Invalid operand"),
+                    }
+                }
             }
             println!("{:#?}", self);
             i += 1;
         }
-
         result
             .iter()
             .map(|x| x.to_string())
             .collect::<Vec<String>>()
             .join(",")
     }
-
-    fn execute_div(&mut self, op: Operand, target: RegisterField) {
-        match target {
-            RegisterField::A => {
-                let value = op.get_value(self);
-                self.a = self.a / 2i32.pow(value as u32);
-            }
-            RegisterField::B => {
-                let value = op.get_value(self);
-                self.b = self.b / 2i32.pow(value as u32);
-            }
-            RegisterField::C => {
-                let value = op.get_value(self);
-                self.c = self.c / 2i32.pow(value as u32);
-            }
-        }
-    }
 }
-
 pub fn solve(input: &str) -> String {
     let input: Vec<&str> = input.trim().split("\n\n").collect();
     let registers: Vec<i32> = input[0]
@@ -138,9 +129,7 @@ pub fn solve(input: &str) -> String {
                 .unwrap()
         })
         .collect();
-
     let mut reg = Register::new(registers[0], registers[1], registers[2]);
-
     let instructions: Vec<(i32, i32)> = input[1].split(" ").collect::<Vec<&str>>()[1]
         .split(",")
         .map(|x| x.parse().unwrap())
@@ -148,20 +137,16 @@ pub fn solve(input: &str) -> String {
         .chunks(2)
         .map(|pair| (pair[0], pair[1]))
         .collect();
-
     reg.process(instructions)
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_solve() {
         let input = "Register A: 729
 Register B: 0
 Register C: 0
-
 Program: 0,1,5,4,3,0";
         assert_eq!(solve(input), "4,6,3,5,6,3,5,2,1,0".to_string());
     }
