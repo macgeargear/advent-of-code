98cec70dfde71b6272cf20754de0de436654c10a
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-17 22:20:59 +0700

    fix(day-17): test case

--- 2024/rust/day-17/src/part1.rs
+++ 2024/rust/day-17/src/part1.rs
@@ -147,6 +147,7 @@ mod tests {
         let input = "Register A: 729
 Register B: 0
 Register C: 0
+
 Program: 0,1,5,4,3,0";
         assert_eq!(solve(input), "4,6,3,5,6,3,5,2,1,0".to_string());
     }
