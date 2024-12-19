e181c5aa14cf968c265e8aa272a6d10dd5b41e5d
Gear <84141000+macgeargear@users.noreply.github.com>
2024-12-19 15:11:49 +0700

    feat(day-19): part2

--- 2024/rust/day-19/src/part2.rs
+++ 2024/rust/day-19/src/part2.rs
@@ -1,5 +1,30 @@
-pub fn solve(input: &str) -> i32 {
-    todo!("solve part 2");
+use std::collections::*;
+
+fn cnt_form(input: &str, vowels: &HashSet<&str>) -> u64 {
+    let n = input.len();
+    let mut dp: Vec<u64> = vec![0; n + 1]; // dp(i) = can form input[0..i] using vowels
+    dp[n] = 1;
+
+    for i in (0..n).rev() {
+        for vowel in vowels {
+            if i + vowel.len() <= n && &input[i..i + vowel.len()] == *vowel {
+                dp[i] += dp[i + vowel.len()]
+            }
+        }
+    }
+
+    dp[0]
+}
+
+pub fn solve(input: &str) -> u64 {
+    let input: Vec<&str> = input.split("\n\n").collect();
+    let vowels: HashSet<&str> = input[0].split(", ").collect();
+    let vocabs: Vec<&str> = input[1].trim().split("\n").collect();
+
+    vocabs
+        .iter()
+        .map(|vocab| cnt_form(vocab, &vowels))
+        .sum::<u64>()
 }
 
 #[cfg(test)]
@@ -8,7 +33,16 @@ mod tests {
 
     #[test]
     fn test_solve() {
-        let input = "1\n2\n3";
-        assert_eq!(solve(input), 0);
+        let input = "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb";
+        assert_eq!(solve(input), 16);
     }
 }
